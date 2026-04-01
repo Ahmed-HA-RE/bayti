f123b167ad8c7ba8f3225e57a81c4981cfa7b9ba
Ahmed=Haitham <ah607k@gmail.com>
2026-04-01 14:36:35 +0400

    feat: add isFavorite field to Property model and favorites UI

--- components/property/property-header-section.tsx
+++ components/property/property-header-section.tsx
@@ -1,24 +1,51 @@
+'use client';
+
 import { Property } from '@/lib/generated/prisma/client';
 import { MotionPreset } from '../shared/motion-preset';
 import { Separator } from '../ui/separator';
-import { formatPrice } from '@/lib/utils';
+import { cn, formatPrice } from '@/lib/utils';
 import { PiMapPinFill } from 'react-icons/pi';
+import { Button } from '../ui/button';
+import { FaHeart } from 'react-icons/fa6';
+import { useState } from 'react';
 
 const PropertyHeaderSection = ({ property }: { property: Property }) => {
+  const isFavorite = property.isFavorite;
+
   return (
     <section className='section-top-spacing'>
       <div className='container'>
-        <div className='flex flex-col gap-4'>
-          <MotionPreset
-            component='h1'
-            fade
-            blur
-            slide={{ direction: 'left' }}
-            delay={0.1}
-            className='text-4xl md:text-5xl'
-          >
-            {property.name}
-          </MotionPreset>
+        <div className='grid grid-cols-1 gap-4'>
+          <div className='flex flex-col md:flex-row items-start md:items-center justify-between gap-4'>
+            <MotionPreset
+              component='h1'
+              fade
+              blur
+              slide={{ direction: 'left' }}
+              delay={0.1}
+              className='text-4xl md:text-5xl'
+            >
+              {property.name}
+            </MotionPreset>
+            <Button
+              className={cn(
+                'rounded-sm',
+                isFavorite &&
+                  'bg-[#FFE4E6] text-[#E11D48] hover:bg-[#FECDD3] hover:text-[#BE123C]',
+              )}
+              size='sm'
+            >
+              <FaHeart
+                className={cn(
+                  isFavorite
+                    ? 'text-[#E11D48] hover:text-[#BE123C]'
+                    : 'text-white',
+                  'size-4',
+                )}
+              />
+              {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+            </Button>
+          </div>
 
           <MotionPreset
             fade
